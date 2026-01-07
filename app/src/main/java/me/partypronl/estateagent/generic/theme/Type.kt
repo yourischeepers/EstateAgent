09e0cb403106a53e19dd4604b29b6e3ab64b1d5d
Youri Scheepers <youri.scheepers@framna.com>
2026-01-07 18:03:17 +0100

    Setup font

--- app/src/main/java/me/partypronl/estateagent/generic/theme/Type.kt
+++ app/src/main/java/me/partypronl/estateagent/generic/theme/Type.kt
@@ -2,21 +2,34 @@ package me.partypronl.estateagent.generic.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import me.partypronl.estateagent.R
 
-// Set of Material typography styles to start with
+val Inter = FontFamily(
+    Font(R.font.inter_thin, FontWeight.Thin),
+    Font(R.font.inter_extra_light, FontWeight.ExtraLight),
+    Font(R.font.inter_light, FontWeight.Light),
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semi_bold, FontWeight.SemiBold),
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.inter_black, FontWeight.Black),
+)
+
 val Typography = Typography(
     bodyMedium = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Inter,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 17.sp,
         letterSpacing = 0.41.sp
     ),
     titleSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Inter,
         fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         lineHeight = 19.sp,
