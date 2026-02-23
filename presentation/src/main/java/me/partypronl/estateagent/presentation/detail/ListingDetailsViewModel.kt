40eeefb64ec20cdd83f067aac0d88d05d9567b44
Youri Scheepers <youri.scheepers@framna.com>
2026-02-23 16:54:13 +0100

    Feat: Zoom map in on home when selecting it

--- presentation/src/main/java/me/partypronl/estateagent/presentation/detail/ListingDetailsViewModel.kt
+++ presentation/src/main/java/me/partypronl/estateagent/presentation/detail/ListingDetailsViewModel.kt
@@ -1,6 +1,8 @@
 package me.partypronl.estateagent.presentation.detail
 
 import androidx.lifecycle.ViewModel
+import me.partypronl.estateagent.presentation.root.map.controller.RootMapController
+import me.partypronl.estateagent.presentation.root.map.controller.RootMapZoom
 import me.partypronl.estateagent.presentation.util.MutableEventFlow
 import me.partypronl.estateagent.presentation.util.asEventFlow
 import me.partypronl.estateagent.presentation.util.emitEvent
@@ -10,12 +12,32 @@ import org.koin.core.annotation.InjectedParam
 @KoinViewModel
 class ListingDetailsViewModel(
     @InjectedParam private val args: ListingDetailsArgs,
+    private val rootMapController: RootMapController,
 ) : ViewModel() {
 
     private val _navigation = MutableEventFlow<ListingDetailsNavigation>()
     val navigation = _navigation.asEventFlow()
 
+    init {
+        moveMapToHome()
+    }
+
     fun onBackClicked() {
         _navigation.emitEvent(ListingDetailsNavigation.GoBack)
+    }
+
+    private fun moveMapToHome() {
+        args.home.details?.let {
+            rootMapController.goToLocation(
+                lat = it.location.lat + LatOffset,
+                lon = it.location.lon,
+                zoomLevel = RootMapZoom.Home,
+            )
+        }
+    }
+
+    companion object {
+
+        const val LatOffset = -0.008
     }
 }
